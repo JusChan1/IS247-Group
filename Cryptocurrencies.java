7e1b04e26befec4336477f052b5463ddb59356c7
JusChan1 <juschan1@umbc.edu>
2017-12-07 15:55:14 -0500

    Update Cryptocurrencies.java

--- Cryptocurrencies.java
+++ Cryptocurrencies.java
@@ -3,7 +3,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
-
 public class Cryptocurrencies {
 
     public static void main(String[] args) {
@@ -18,7 +17,6 @@ public class Cryptocurrencies {
         Cryptocurrency steem = new Cryptocurrency("Steem", 1, "social coin");
 
         //Using Map and HashMap
-
         Map<String, String> wallet = new HashMap<>();
         wallet.put("Bitcoin", "major cryptocurrency");
         wallet.put("Ether", "major cryptocurrency");
@@ -28,8 +26,6 @@ public class Cryptocurrencies {
         wallet.put("Bitcoin Cash", "derivative");
         wallet.put("Bitcoin Gold", "derivative");
         wallet.put("Steem", "social coin");
-
-        printWallet(wallet);
 
         //Using List
         Collection<Cryptocurrency> coins = new ArrayList<>();
@@ -41,36 +37,67 @@ public class Cryptocurrencies {
         coins.add(bitcash);
         coins.add(bitgold);
         coins.add(steem);
-        /*
-        for (Cryptocurrency coin : coins) {
-            System.out.println("coin: " + coin.getName() + " || value(USD): " + coin.getValue() + " || type: " + coin.getType());
-        }
-        System.out.println("-----------------------------------");
-        */
-        printCoinList(coins);
 
         //Using Stack
         Stack coinStack = new Stack();
 
-        for (Cryptocurrency coin : coins ) {
+        for (Cryptocurrency coin : coins) {
             coinStack.push(coin);
         }
-        printCoins(coinStack);
+
+        Scanner input = new Scanner(System.in);            //1) Scanner Class
+        boolean running = true;//keeps menu open
+        while (running) {//look keeps menu running until it closing application
+            displayCoinMenu();    //display menu
+            String userInput = input.nextLine();
+
+            try {
+                // 3) Switch Statement
+                switch (userInput) {
+                    case "1":
+                        printWallet(wallet);
+                        break;
+                    case "2":
+                        printCoins(coinStack);
+                        break;
+                    case "3":
+                        printCoinList(coins);
+                        break;
+                    case "4":
+                        running = false;//closes the application
+                        break;
+                    default:
+                        System.out.println("Invalid selection");//happens if user makes invalid input
+                        break;                                  // 8) Exception
+                }
+            } catch (InputMismatchException a) {
+                System.out.println("Invalid input detected. Please try again.");
+            } catch (ArrayIndexOutOfBoundsException b) {
+                System.out.println("You did not enter a valid account number.");
+            }
+        }
+    }
+    private static void displayCoinMenu() {
+        System.out.println("Enter the number for the service you want to use");
+        System.out.println("1 View Wallet");
+        System.out.println("2 View All Coins");
+        System.out.println("3 View List of Coins");
+        System.out.println("4 Quit");
     }
 
-    public static void printWallet(Map<String, String> wallet) {
+    private static void printWallet(Map<String, String> wallet) {
         System.out.println("Wallet: " + wallet);
         System.out.println("Wallet Size:  " + wallet.size());
         System.out.println("---------------------------------");
         System.out.println("Coins:");
     }
 
-    public static void printCoins(Stack coinStack) {
+    private static void printCoins(Stack coinStack) {
         System.out.println(coinStack);
         System.out.println("All virtual coins displayed.");
     }
 
-    public static void printCoinList(Collection<Cryptocurrency> coins) {
+    private static void printCoinList(Collection<Cryptocurrency> coins) {
         for (Cryptocurrency coin : coins) {
             System.out.println("coin: " + coin.getName() + " || value(USD): " + coin.getValue() + " || type: " + coin.getType());
         }
